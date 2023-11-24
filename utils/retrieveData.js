163147ea607326242c618a93b18d3216847a0f42
Nathan Pham <nghiapham102603@gmail.com>
2023-11-23 23:18:38 -0800

    Caching for retrieveData

--- utils/retrieveData.js
+++ utils/retrieveData.js
@@ -1,3 +1,5 @@
+const cache = require('./cache'); // Assuming cache.js is in the same directory
+
 const retrieveDataFromDb = async (
   Model,
   fetchFunction,
@@ -6,34 +8,45 @@ const retrieveDataFromDb = async (
   errorMessage
 ) => {
   try {
-    let data;
-    if (!queryParams || Object.keys(queryParams).length === 0) {
-      // If queryParams is null or an empty object, retrieve without query
-      data = await Model.findOne({});
+    // Generate a unique cache key based on the model name and queryParams
+    const cacheKey = `${Model.modelName}-${JSON.stringify(queryParams)}`;
+
+    // Try to get data from cache
+    let data = cache.get(cacheKey);
+
+    // If data is not in cache or cache data is stale, fetch from DB
+    if (!data) {
+      if (!queryParams || Object.keys(queryParams).length === 0) {
+        data = await Model.findOne({});
+      } else {
+        data = await Model.findOne({ queryParams });
+      }
+
+      const currentTime = new Date();
+      const twentyFourHoursAgo = new Date(currentTime - 24 * 60 * 60 * 1000);
+
+      // If there's no data or the data's updatedAt date is more than 24 hours old
+      // fetch new data and update the cache
+      if (!data || (data.updatedAt && data.updatedAt < twentyFourHoursAgo)) {
+        const fetchedData = await fetchAndSaveToDb(fetchFunction, Model, queryParams);
+        // Update cache with new data using stdTTL as the TTL
+        cache.set(cacheKey, JSON.stringify(fetchedData), 3600); // 3600 seconds TTL
+        data = fetchedData;
+      } else {
+        // Data is fresh enough, update the cache
+        cache.set(cacheKey, JSON.stringify(data), 3600);
+      }
+    }
+
+    // If data exists, return it
+    if (data) {
+      res.status(200).json(data);
     } else {
-      // Else, use queryParams to retrieve data
-      data = await Model.findOne({ queryParams });
+      // Handle no data found
+      res.status(404).json({ message: errorMessage || 'Data not found' });
     }
-
-    const currentTime = new Date();
-    const twentyFourHoursAgo = new Date(currentTime - 24 * 60 * 60 * 1000); // 24 hours ago in milliseconds
-
-    // If there's no data or the data's updatedAt date is more than 24 hours old, fetch and save new data
-    if (!data || (data.updatedAt && data.updatedAt < twentyFourHoursAgo)) {
-      await fetchAndSaveToDb(fetchFunction, Model, queryParams);
-      // Call itself recursively to get the newly saved data
-      return retrieveDataFromDb(
-        Model,
-        fetchFunction,
-        queryParams,
-        res,
-        errorMessage
-      );
-    }
-
-    res.status(200).json(data);
   } catch (error) {
-    console.error('Error fetching data from MongoDB:', error);
+    console.error('Error fetching data:', error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 };
@@ -46,7 +59,7 @@ const fetchAndSaveToDb = async (fetchFunction, Model, queryParams) => {
     return newData;
   } catch (error) {
     console.error('Error fetching and saving data:', error);
-    return null;
+    throw error; // Rethrow the error to be handled by the caller
   }
 };
 
