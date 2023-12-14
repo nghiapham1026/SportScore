d2d0ef9013d1aa6d8d0b6d6087eb41c9ce7db954
Nathan Pham <nghiapham102603@gmail.com>
2023-12-13 20:05:53 -0800

    Daily Health Check

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const cors = require('cors');
+const cron = require('node-cron');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 
+// Import your routes
 const leaguesRoutes = require('./routes/leagues');
 const teamsRoutes = require('./routes/teams');
 const venuesRoutes = require('./routes/venues');
@@ -13,11 +16,13 @@ const standingsRoutes = require('./routes/standings');
 const fixturesRoutes = require('./routes/fixtures');
 const playersRoutes = require('./routes/players');
 
+// Connect to the database
 mongoose
   .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to database'))
   .catch((err) => console.error('Failed to connect to database', err));
 
+// Set up routes
 app.use('/leagues', leaguesRoutes);
 app.use('/teams', teamsRoutes);
 app.use('/venues', venuesRoutes);
@@ -25,12 +30,30 @@ app.use('/standings', standingsRoutes);
 app.use('/fixtures', fixturesRoutes);
 app.use('/players', playersRoutes);
 
-app.get('/', (_, res) => {
-  res.send(
-    'SportScore back-end server, view manual for a list of all endpoints'
-  );
+// Health check endpoint
+app.get('/healthcheck', (_, res) => {
+    // Perform necessary health checks here
+    res.status(200).send('Server is healthy');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running`);
+// Home route
+app.get('/', (_, res) => {
+  res.send('SportScore back-end server, view manual for a list of all endpoints');
 });
+
+// Schedule a task to run every day at 8:00 AM
+cron.schedule('0 0 * * *', async () => {
+    try {
+        // Replace with your server's health check endpoint
+        const response = await axios.get(`${process.env.SERVER_URL}/healthcheck`);
+        console.log('Daily health check successful:', response.status);
+    } catch (err) {
+        console.error('Error during daily health check:', err);
+    }
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running`);
+});
+  
